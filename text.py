6664c84ea00e768bb8b940e9afbc11f56d15c64c
DekkoBraze <56168611+DekkoBraze@users.noreply.github.com>
2024-02-01 19:37:16 +0300

    Update text.py

--- text.py
+++ text.py
@@ -1,2 +1,2 @@
-greet = "Hello, {name}! I'm RandomMusicBot. You can explore music with my help. Press button below to enter a tag and start searching.?"
+greet = "Hello, {name}! I'm RandomMusicBot. You can explore music with my help. Press button below to enter a tag and start searching."
 get_tag = 'Write a tag or choose it from a menu. It can be genre or mood.'
